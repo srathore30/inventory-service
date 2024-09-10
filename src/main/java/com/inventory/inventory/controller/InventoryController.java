d9ae3cf45d07921e0853277fa6728fc1c3ae179b
Vivek Pandey <vivekpandey1464@gmail.com>
2024-09-10 21:38:30 +0530

    token auth addded

--- src/main/java/com/inventory/inventory/controller/InventoryController.java
+++ src/main/java/com/inventory/inventory/controller/InventoryController.java
@@ -5,6 +5,7 @@ import com.inventory.inventory.dto.request.InventoryUpdateRequest;
 import com.inventory.inventory.dto.response.InventoryResponse;
 import com.inventory.inventory.dto.response.InventoryUpdateResponse;
 import com.inventory.inventory.dto.response.PaginatedResp;
+import com.inventory.inventory.interceptor.UserAuthorization;
 import com.inventory.inventory.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,16 +17,19 @@ import org.springframework.web.bind.annotation.*;
 public class InventoryController {
     private final InventoryService inventoryService;
     @PostMapping("/inventory/create")
+    @UserAuthorization
     public ResponseEntity<InventoryResponse> updateInventory(@RequestBody InventoryRequest request) {
         return new ResponseEntity<>(inventoryService.createInventory(request), HttpStatus.OK);
     }
 
     @PutMapping("/inventory/update/{id}")
+    @UserAuthorization
     public ResponseEntity<InventoryUpdateResponse> updateInventory( @PathVariable Long id,@RequestBody InventoryUpdateRequest request) {
         return new ResponseEntity<>(inventoryService.updateInventory(id,request), HttpStatus.OK);
     }
 
     @GetMapping("/inventory/{productId}")
+    @UserAuthorization
     public ResponseEntity<PaginatedResp<InventoryResponse>> getInventoryByProductId(@PathVariable Long productId, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int pageSize, @RequestParam(defaultValue = "createdDate") String sortBy, @RequestParam(defaultValue = "desc") String sortDirection) {
         return new ResponseEntity<>(inventoryService.getInventory(productId, page, pageSize, sortBy, sortDirection), HttpStatus.OK);
 
