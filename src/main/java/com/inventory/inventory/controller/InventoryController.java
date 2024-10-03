8df2fae38a4cd67ec058ae5619eaa9538c716a9f
MishraDurgesh1 <durgesh@coad-aspire.com>
2024-10-03 12:17:31 +0530

    auth and cors handle

--- src/main/java/com/inventory/inventory/controller/InventoryController.java
+++ src/main/java/com/inventory/inventory/controller/InventoryController.java
@@ -21,30 +21,32 @@ public class InventoryController {
     private final InventoryService inventoryService;
 
     @PostMapping("/inventory/create")
-//    @UserAuthorization(allowedRoles = {UserRole.Edit_Manager})
+    @UserAuthorization(allowedRoles = {UserRole.Create_Manager, UserRole.Edit_Manager, UserRole.Delete_Manager,UserRole.View_Manager,UserRole.Manager})
     public ResponseEntity<InventoryResponse> updateInventory(@RequestBody InventoryRequest request) {
         return new ResponseEntity<>(inventoryService.createInventory(request), HttpStatus.OK);
     }
 
     @PutMapping("/inventory/update/{id}")
-//    @UserAuthorization(allowedRoles = {UserRole.Manager})
+    @UserAuthorization(allowedRoles = {UserRole.Create_Manager, UserRole.Edit_Manager, UserRole.Delete_Manager,UserRole.View_Manager,UserRole.Manager})
     public ResponseEntity<InventoryUpdateResponse> updateInventory(@PathVariable Long id, @RequestBody InventoryUpdateRequest request) {
         return new ResponseEntity<>(inventoryService.updateInventory(id, request), HttpStatus.OK);
     }
 
 
     @GetMapping("/inventory/{productId}")
+    @UserAuthorization(allowedRoles = {UserRole.Create_Manager, UserRole.Edit_Manager, UserRole.Delete_Manager,UserRole.View_Manager,UserRole.Manager})
     public ResponseEntity<PaginatedResp<InventoryResponse>> getInventoryByProductId(@PathVariable Long productId, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int pageSize, @RequestParam(defaultValue = "createdDate") String sortBy, @RequestParam(defaultValue = "desc") String sortDirection) {
         return new ResponseEntity<>(inventoryService.getInventory(productId, page, pageSize, sortBy, sortDirection), HttpStatus.OK);
     }
 
     @PutMapping("/inventory/fill/{id}")
-//    @UserAuthorization(allowedRoles = {UserRole.Manager})
+    @UserAuthorization(allowedRoles = {UserRole.Create_Manager, UserRole.Edit_Manager, UserRole.Delete_Manager,UserRole.View_Manager,UserRole.Manager})
     public ResponseEntity<InventoryFillResponse> fillInventory(@PathVariable Long id, @RequestBody InventoryFillRequest request) {
         return new ResponseEntity<>(inventoryService.fillInventory(id, request), HttpStatus.OK);
     }
 
     @GetMapping("/inventory/all")
+    @UserAuthorization(allowedRoles = {UserRole.Create_Manager, UserRole.Edit_Manager, UserRole.Delete_Manager,UserRole.View_Manager,UserRole.Manager})
     public ResponseEntity<PaginatedResp<InventoryResponse>> getAllInventory(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int pageSize, @RequestParam(defaultValue = "createdDate") String sortBy, @RequestParam(defaultValue = "desc") String sortDirection) {
         return new ResponseEntity<>(inventoryService.getAllInventory(page, pageSize, sortBy, sortDirection), HttpStatus.OK);
     }
